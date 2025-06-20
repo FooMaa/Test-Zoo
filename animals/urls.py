278059ff47a6a53b84469d684e22ef1bb004f8d6
anton <avodichenkov@gmail.com>
2025-06-20 13:16:42 +0300

    Поправил разный стиль URL для процедур

--- animals/urls.py
+++ animals/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('animals/<int:pk>/', views.AnimalDetailView.as_view(), name='animal'),
     path('animals/create/', views.AnimalCreateView.as_view(), name='animal-create'),
     path('animals/<int:pk>/update/', views.AnimalUpdateView.as_view(), name='animal-update'),
-    path('animals/<int:pk>/add_procedure/', views.ProcedureCreateView.as_view(), name='add-procedure'),
+    path('animals/<int:pk>/procedures/create', views.ProcedureCreateView.as_view(), name='add-procedure'),
 ]
